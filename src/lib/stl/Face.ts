23d7c561952a3a79aaaceea0eee2f6f766e10103
Mike Lee <mikemunkyu.lee@gmail.com>
2021-04-21 22:41:48 -0400

    fix: specify Point3 as type for Face vertices

--- src/lib/stl/Face.ts
+++ src/lib/stl/Face.ts
@@ -1,6 +1,8 @@
+import { Point3 } from './Point3';
+
 export class Face {
-  v: number[] = [];
-  constructor(v0: number, v1: number, v2: number) {
+  v: Point3[] = [];
+  constructor(v0: Point3, v1: Point3, v2: Point3) {
     this.v[0] = v0;
     this.v[1] = v1;
     this.v[2] = v2;
