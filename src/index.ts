8c760ea795807013e2afd61d59adaa6572998603
Mike Lee <mikemunkyu.lee@gmail.com>
2021-04-22 20:08:23 -0400

    feat: add demo script to test run loading STL file

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,19 @@
 // TODO: example of how lib should be exposed
 // export * from './lib/async';
 
-// Demo
-const demo = () => {
-  console.log('hello world!');
-}
+import * as path from 'path';
 
-demo()
+import { Loader } from './lib/stl/Loader';
+
+// Demo
+const demo = async () => {
+  console.log('hello world!');
+
+  const loader = new Loader();
+  const filePath = path.resolve('src/assets/teapot.stl');
+  const loadedModel = await loader.loadBinarySTL(filePath);
+
+  console.log('loaded', loadedModel);
+};
+
+demo();
